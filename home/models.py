b7cebc4fcba2f9d0a9ce8e1b13632a814b536172
ecakashjadhav <akash.jadhav@ecinfosolutions.com>
2022-05-04 15:23:48 +0530

    commit

---
+++ home/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+import json
+
+class Notifications(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    notifications = models.TextField(max_length=200)
+    is_seen = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        channel_layer = get_channel_layer()
+        notification_objs = Notifications.objects.filter(is_seen=False).count()
+        data = {'count':notification_objs, 'current_notification':self.notifications}
+        async_to_sync(channel_layer.group_send)(
+            'test_consumer_group', {
+                'type':'send_notification',
+                'value':json.dumps(data)
+            }
+        )
+        super(Notifications,self).save(*args, **kwargs)
+
